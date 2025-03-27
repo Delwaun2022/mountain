431498d6cc6fa4b32ac14ba4bbe8a42ee2850d93
delwaun2022 <delwaunb@yahoo.com>
2025-03-27 02:17:00 +0000

    Update UI - enhancing logs

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-const fs = require('fs-extra')
-const path = require('path')
-const json5 = require('json5')
-const cwd = process.cwd()
+let fs = require('fs-extra')
+let path = require('path')
+let json5 = require('json5')
+let cwd = process.cwd()
 
 module.exports = api => {
   if (api) api.cache(true)
