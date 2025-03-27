024764c8b3bdae397140a5666b9f5f3dbd2ab9a2
delwaun2022 <delwaunb@yahoo.com>
2025-03-27 02:54:15 +0000

    Remove database - enhancing logs

--- packages/react/src/hooks/useMutlipleProvider.ts
+++ packages/react/src/hooks/useMutlipleProvider.ts
@@ -4,22 +4,22 @@ import { ListLifeCycleTypes } from '@alist/core'
 import useForceUpdate from '../hooks/useForceUpdate'
 import { IMultipleProps, IMultipleHook } from '../types'
 
-export const useMultipleProvider = (props: IMultipleProps): IMultipleHook => {
-    const { id, pageSize = 10 } = props
-    const list = useContext(ListContext)
+export let useMultipleProvider = (props: IMultipleProps): IMultipleHook => {
+    let { id, pageSize = 10 } = props
+    let list = useContext(ListContext)
 
     useEffect(() => {
         list && list.setMultiplePageSize({ [id]: pageSize })
     }, [])
 
-    const forceUpdate = useForceUpdate()
-    const refresh = () => {
+    let forceUpdate = useForceUpdate()
+    let refresh = () => {
         forceUpdate()
     }
     
     useEffect(() => {
         if (list) {
-            const id = list.subscribe(ListLifeCycleTypes.ON_LIST_MULTIPLE_REFRESH, refresh)
+            let id = list.subscribe(ListLifeCycleTypes.ON_LIST_MULTIPLE_REFRESH, refresh)
             return function cleanup() {
                 list.unSubscribe(id)
             }
